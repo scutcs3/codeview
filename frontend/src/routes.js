966b5393cb10198037c8109777652c6da19f4f10
Timmmza <83821536+Timmmza@users.noreply.github.com>
2021-05-13 22:19:03 +0800

    注册界面添加

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,7 @@ import Index from './pages/Index.vue'
 import Login from './pages/Login.vue'
 import Viewee from './pages/Viewee.vue'
 import Viewer from './pages/Viewer.vue'
-
+import Register from './pages/Register.vue'
 
 
 let routes = [
@@ -28,6 +28,11 @@ let routes = [
         path: '/viewee',
         component: Viewee,
         name: 'viewee',
+    },
+    {
+        path: '/register',
+        component: Register,
+        name: 'register',
     }
 ]
 
