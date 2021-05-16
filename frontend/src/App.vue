c9276105f0012e618e3e10f6fd0ead3e9946d88c
huangzhiquan <huangzhiquan@bytedance.com>
2021-05-17 00:27:46 +0800

    跳转到注册界面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,9 +5,13 @@
             <router-link to="/login" class="link">登录界面</router-link>
             <router-link to="/register" class="link">注册界面</router-link>
             <router-link to="/viewee" class="link">面试者界面</router-link>
-            <router-link to="/viewer" class="link">面试官界面</router-link> 
+            <router-link to="/viewer" class="link">面试官界面</router-link>
         </ul>
-        <router-view />
+        <router-view v-slot="{ Component }">
+            <keep-alive>
+                <component :is="Component" />
+            </keep-alive>
+        </router-view>
     </div>
 </template>
 <style scoped>
