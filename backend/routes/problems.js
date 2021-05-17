eeddbcd5f70446bc368acb64de0a6c04d42a72f4
MouseHappy123 <1351923644@qq.com>
2021-05-17 16:02:16 +0800

    fix mysql connect bug :bug:

--- backend/routes/problems.js
+++ backend/routes/problems.js
@@ -2,17 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 var connection = require('../config/mysql');
+// connect test
+connection.connect(function(err) {
+    if (err) {
+      console.error('[connect error]: ' + err.stack);
+      return;
+    }
+    console.log('[connect success]: id ' + connection.threadId);
+});
+
 /* GET problems page. */
 router.get('/:id', function (req, res, next) {
     var sql = `SELECT * FROM qb where id=${req.params.id}`;
     connection.query(sql, function (err, result) {
         if (err) {
             console.log('[SELECT ERROR]:', err.message);
+            res.json({
+                data: '[SELECT ERROR]:'+ err.message,
+                status: 'fail'
+            })
+        }else{
+            res.json({
+                data: result[0],
+                status: 'success'
+            })
         }
-        res.json({
-            data: result[0],
-            status: 'success'
-        })
     });
 });
 
