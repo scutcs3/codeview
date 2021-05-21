ec83ba7b6404595a507065b31388204ce0fee292
tootal <tootal@yeah.net>
2021-05-22 00:35:24 +0800

    添加接口handle

--- frontend/src/api/global.js
+++ frontend/src/api/global.js
@@ -1,1 +1,12 @@
 export const baseUrl = 'https://virtserver.swaggerhub.com/tootal/codeview/1.0.0';
+
+export class Handler {
+    constructor(res) {
+        this.res = res;
+    }
+    handle = (callback) => {
+        this.res.then(function({ status, headers, data }) {
+            callback[status](data, headers);
+        })
+    }
+}
