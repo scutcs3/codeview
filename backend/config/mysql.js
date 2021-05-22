2a0b311be241cb55ee6386662d40f4ed5f158959
tootal <tootal@yeah.net>
2021-05-22 20:15:34 +0800

    加入mysql本地配置

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,11 +1,21 @@
 var mysql = require('mysql');
+var user = 'root';
+var password = '';
+var database = '123';
+
+try {
+    local = require('./mysql.local.js');
+    if (local.user) user = local.user;
+    if (local.password) password = local.password;
+    if (local.database) database = local.database;
+} catch (e) {}
 
 var connection = mysql.createConnection({      //创建mysql实例
     host:'127.0.0.1',
     port:'3306',
-    user:'root',
-    password:'',
-    database:'123'
+    user,
+    password,
+    database,
 });
 
 module.exports = connection;
