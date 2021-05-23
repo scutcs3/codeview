0061872b91d8f273cb876988bcc2d89f2a7d0380
MouseHappy123 <1351923644@qq.com>
2021-05-23 22:13:08 +0800

    调整数据库表文件

--- backend/config/interview_problem.sql
+++
@@ -1,8 +0,0 @@
-create table if not EXISTS interview_problem
-(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    interview_id int not null,
-    problem_id int not null,
-    FOREIGN KEY (interview_id) REFERENCES interview(id),
-    FOREIGN KEY (problem_id) REFERENCES problem(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
