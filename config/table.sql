26918bc88ebefd0ef78e70178f491cbd39d37684
tootal <tootal@yeah.net>
2021-05-28 00:32:23 +0800

    放开创建面试限制

--- config/table.sql
+++ config/table.sql
@@ -23,9 +23,9 @@ create table if not EXISTS interview
 (
     id int PRIMARY KEY AUTO_INCREMENT,
     viewer_id int not null,
-    viewee_id int not null,
-    start_time datetime not null,
-    finish_time DATETIME not null,
+    viewee_id int,
+    start_time datetime,
+    finish_time datetime,
     status ENUM('created', 'ready', 'cancelled', 'rejected') not null,
     FOREIGN KEY (viewer_id) REFERENCES user(id),
     FOREIGN KEY (viewee_id) REFERENCES user(id)
