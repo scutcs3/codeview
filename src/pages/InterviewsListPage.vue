0b41718d05372f0fe6bb2cd0c5da44941b268246
tootal <tootal@yeah.net>
2021-05-29 00:55:30 +0800

    新建面试列表

--- src/pages/InterviewsListPage.vue
+++ src/pages/InterviewsListPage.vue
@@ -2,25 +2,55 @@
   <div>
     <h2>面试列表</h2>
     <el-button @click="newInterviews">创建面试</el-button>
-    <div class="view-cards">
-      <ViewCard
-        v-for="i in 10"
-        :key="i"
-        class="view-card"
-        :title="'第' + i + '场面试'"
-      ></ViewCard>
+    <el-table
+      :data="
+        tableData.slice(
+          (dictCurrentPage - 1) * dictPageSize,
+          dictCurrentPage * dictPageSize
+        )
+      "
+      highlight-current-row
+      border
+      style="width: 100%"
+    >
+      <el-table-column label="面试ID" v-slot="{ row }" width="200">
+        <router-link :to="{ name: 'viewer', params: { id: row.hashid } }">
+          面试 #{{ row.id }}
+        </router-link>
+      </el-table-column>
+      <el-table-column prop="start_time" label="开始时间" width="200">
+      </el-table-column>
+      <el-table-column prop="finish_time" label="结束时间" width="200">
+      </el-table-column>
+      <el-table-column prop="status" label="当前状态" width="100">
+      </el-table-column>
+    </el-table>
+    <div class="pages">
+      <el-pagination
+        class="fy"
+        layout="sizes, prev, pager, next, total"
+        v-model="dictCurrentPage"
+        :pager-count="9"
+        background
+        :total="tableData.length"
+        :page-size="30"
+        :page-sizes="[30, 50, 100]"
+      >
+      </el-pagination>
     </div>
   </div>
 </template>
 <script>
-import ViewCard from "../components/ViewCard.vue";
+import { ElMessage } from "element-plus";
+import { getInterviews } from "../api/interview";
 export default {
   name: "InterviewsList",
-  components: {
-    ViewCard,
-  },
   data() {
     return {
+      tableData: [],
+      currentPage: 1,
+      dictCurrentPage: 1,
+      dictPageSize: 30,
       props: {
         fieldValue: [String, Boolean],
       },
@@ -62,6 +92,27 @@ export default {
       });
     },
   },
+  activated() {
+    const self = this;
+    getInterviews().handle({
+      200: (data) => {
+        self.tableData = [];
+        for (let interview of data) {
+          self.tableData.push({
+            id: interview.id,
+            start_time: interview.start_time || "待定",
+            finish_time: interview.finish_time || "待定",
+            status: interview.status,
+          });
+        }
+      },
+      401: () => {
+        ElMessage.warning("登录信息失效，请重新登录！");
+        this.$router.push("/login");
+      },
+      404: () => ElMessage.warning("获取面试列表失败！"),
+    });
+  },
 };
 </script>
 <style scoped>
@@ -72,5 +123,9 @@ export default {
 }
 .view-card {
   margin: 1rem;
+}
+.pages {
+  margin: 2rem auto;
+  text-align: center;
 }
 </style>
