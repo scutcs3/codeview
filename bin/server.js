115a08fafc4d2b0c24f42ef4fd32a361c927d561
MouseHappy123 <1351923644@qq.com>
2021-05-29 18:12:15 +0800

    websocket server send msg divide by iid

--- bin/server.js
+++ bin/server.js
@@ -1,15 +1,26 @@
 const ws = require("nodejs-websocket");
 const PORT = 3002;
 let num = 0;
+
+let conns = {};
+
+function boardcast(obj) {
+  if (obj.interviewID) {
+    conns[obj.interviewID].sendText(JSON.stringify(obj));
+  }
+  return;
+}
+
 const serve = ws.createServer(function (connect) {
   let id = num;
   console.log("用户" + id + "连接上来!");
   num++;
   //接收到信息函数
-  connect.on("text", function (str) {
-    console.log("接受到: " + str);
-    //connect.sendText(str);//向这个浏览器回发str
-    sentall(str); //向所有连接上来的浏览器发送str
+  connect.on("text", function (obj) {
+    console.log("接受到: " + obj);
+    obj = JSON.parse(obj);
+    conns["" + obj.interviewID + ""] = connect;
+    boardcast(obj);
   });
   //有连接断开函数
   connect.on("close", function () {
@@ -19,11 +30,11 @@ const serve = ws.createServer(function (connect) {
   connect.on("error", function () {
     console.log("用户连接异常");
   });
-  function sentall(str) {
-    serve.connections.forEach(function (connect) {
-      connect.sendText(str);
-    });
-  }
+  //   function sentall(str) {
+  //     serve.connections.forEach(function (connect) {
+  //       connect.sendText(str);
+  //     });
+  //   }
 });
 serve.listen(PORT, function () {
   console.log("启动成功，监听" + PORT + "端口");
