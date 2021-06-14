4114a7a36e55c609e37a3371bf4584df4356ed3e
tootal <tootal@yeah.net>
2021-06-14 20:07:42 +0800

    回退mysql配置

--- config/mysql.js
+++ config/mysql.js
@@ -8,7 +8,6 @@ let database = "123";
 https://github.com/scutcs3/codeview/wiki/%E9%83%A8%E7%BD%B2%E6%B5%81%E7%A8%8B#%E9%85%8D%E7%BD%AE%E6%95%B0%E6%8D%AE%E5%BA%93%E8%BF%9E%E6%8E%A5
 配置本地数据库不要在此修改，在同目录下新建文件`mysql.local.js`，
 里面填入本地数据库的账号、密码、数据库名，例如：
-
 module.exports = {
     user: 'root',
     password: 'test',
@@ -27,11 +26,11 @@ try {
   console.log("未检测到本地配置，使用默认数据库配置！");
 }
 
-var conn;
+var connection;
 var isConnect = false;
 
 if (!isConnect) {
-  let connection = mysql.createConnection({
+  connection = mysql.createConnection({
     //创建mysql实例
     host: "127.0.0.1",
     port: "3306",
@@ -42,14 +41,12 @@ if (!isConnect) {
   // connect test
   connection.connect(function (err) {
     if (err) {
-      console.error("MySQL连接失败，改用SQLite");
-      conn = require("./sqlite");
-    } else {
-      console.log("MySQL连接成功，id = " + connection.threadId);
-      conn = connection;
+      console.error("数据库连接失败: " + err.stack);
+      return;
     }
+    console.log("数据库连接成功，id = " + connection.threadId);
   });
 }
 isConnect = true;
 
-module.exports = conn;
+module.exports = connection;
